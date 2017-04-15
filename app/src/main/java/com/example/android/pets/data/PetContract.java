aeabbe7f06b139682427bc816d51d71836115478
Pack Heng <pack@oz-heng.com>
2017-04-16 07:10:55 +1000

    Create the contract.  On branch L2_001_Create_The_Contract  Changes to be committed: 	modified:   app/src/main/java/com/example/android/pets/EditorActivity.java 	new file:   app/src/main/java/com/example/android/pets/data/PetContract.java

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,34 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Pack Heng on 15/04/17
+ * pack@oz-heng.com
+ */
+
+public final class PetContract {
+
+    /**
+     * An empty constructor to prevent the instantiation of this class.
+     */
+    private PetContract() {}
+
+    public static class PetEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "pets";
+
+        private static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT= "weight";
+
+        /**
+         * Possible values for the gender of the pet.
+         */
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+}
